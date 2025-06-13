d9076e55bf223a310c8cd649b2a08ac7c314658d
kyrathat <kyrajaneyoung001@gmail.com>
2025-06-13 18:04:07 -0500

    initial commit
    initial commit

---
+++ personal finance app/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+
+namespace personal_finance_app
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+        public class myItem
+        {
+            public string Title { get; set; }
+            public string Image { get; set; }
+        }
+
+        public class MainViewModel
+        {
+            public ObservableCollection<myItem> MyItems { get; set; }
+
+            public MainViewModel()
+            {
+                MyItems = new ObservableCollection<myItem>()
+                {
+                    new myItem { Title = "First", Image = "first"},
+                    new myItem { Title = "Second", Image = "second"}
+                };
+            }
+        }
+
+
+    }
+
+}
---
+++ personal finance app/insight/insight.xaml.cs
@@ -0,0 +1,9 @@
+namespace personal_finance_app;
+
+public partial class insight : ContentPage
+{
+	public insight()
+	{
+		InitializeComponent();
+	}
+}
